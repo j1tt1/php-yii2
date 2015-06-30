35aa769723d8f56a31db25de43b98d02851dac88
j1tt1_000 <j1tt1_000@ICAD43>
2015-06-30 16:52:11 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,4 +2,12 @@
 /* @var $this yii\web\View */
 $this->title = 'John Application Yii2';
 ?>
-<div align="center">Hello Yii2</div>
+<?php
+$route1 = Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที่ test1</a><br />
+
+<?php
+$route2 = Yii::$app->urlManager->createUrl(['test/test2','name'=>'John','lname'=>'Noonung']);
+?>
+<a href="<?=$route2?>">ไปที่ test2</a><br />
